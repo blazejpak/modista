76746382fae556724f6de22c2b75338b51433c9b
blazejpak <blazej.pak@gmail.com>
2024-03-13 20:10:44 +0100

    initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {},
+    extend: {
+      colors: {
+        "gold-dark": "#D87D4A",
+        "gold-lighter": "#FBAF85",
+        "black-lighter": "#101010",
+        "grey-normal": "#F1F1F1",
+        "grey-lighter": "#FAFAFA",
+      },
+    },
   },
   plugins: [],
 };
