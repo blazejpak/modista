31603f6ab4c069d5604d29c6f75a35d4cc7e95f5
blazejpak <blazej.pak@gmail.com>
2024-03-28 19:05:50 +0100

    deleted console.logs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,6 +15,7 @@ const Header = () => {
 
     return () => window.removeEventListener("resize", handlerResize);
   }, []);
+
   return (
     <header className="sticky top-0 z-10  w-full select-none  bg-black-lighter  text-grey-normal">
       {isMobile ? <HeaderPhone /> : <HeaderBiggerScreens />}
