40b585904562921cb3097a1df0652abbdc76667c
blazejpak <blazej.pak@gmail.com>
2024-03-28 18:09:25 +0100

    changed links for bestsellers and products with the biggest discount

--- src/routes/CategoryPage/Bestsellers.tsx
+++ src/routes/CategoryPage/Bestsellers.tsx
@@ -1,9 +1,12 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import type { Category } from "../../utils/types";
 import { getDataRatingAndDiscount } from "../../utils/helpers";
+import { categoryLinks } from "./categoryLinks";
 
 const Bestsellers = () => {
   const data = useLoaderData() as Category[];
+  const { category } = useParams();
+  const linkArr = category ? categoryLinks[category] : [];
 
   const highRatedProducts = getDataRatingAndDiscount(data, "discount");
 
@@ -14,28 +17,34 @@ const Bestsellers = () => {
           Check ours bestsellers!
         </h2>
         <div className="flex flex-wrap justify-center gap-4 lg:justify-start lg:gap-8 ">
-          {highRatedProducts.map(
-            (product) =>
-              product && (
-                <Link
-                  key={product.id}
-                  to={product.id.toString()}
-                  className="group relative h-[350px] w-[250px] overflow-hidden rounded-xl bg-black-lighter shadow transition-transform focus:scale-105"
-                >
-                  {product.images[0] && (
-                    <img
-                      alt={product.description}
-                      src={product.images[0]}
-                      height={300}
-                      className="h-[250px] w-full "
-                    />
-                  )}
-                  <p className=" p-6 text-center uppercase text-grey-lighter transition-colors group-hover:text-gold-dark ">
-                    {product.title}
-                  </p>
-                </Link>
-              ),
-          )}
+          {highRatedProducts.map((product) => {
+            if (product) {
+              if (linkArr) {
+                const link = linkArr.find(
+                  (item) => item.fullName === product.category,
+                );
+                return (
+                  <Link
+                    key={product.id}
+                    to={`/${link?.link}/${product.id.toString()}`}
+                    className="group relative h-[350px] w-[250px] overflow-hidden rounded-xl bg-black-lighter shadow transition-transform focus:scale-105"
+                  >
+                    {product.images[0] && (
+                      <img
+                        alt={product.description}
+                        src={product.images[0]}
+                        height={300}
+                        className="h-[250px] w-full "
+                      />
+                    )}
+                    <p className=" p-6 text-center uppercase text-grey-lighter transition-colors group-hover:text-gold-dark ">
+                      {product.title}
+                    </p>
+                  </Link>
+                );
+              }
+            }
+          })}
         </div>
       </section>
     );
--- src/routes/CategoryPage/Discount.tsx
+++ src/routes/CategoryPage/Discount.tsx
@@ -1,9 +1,13 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import type { Category } from "../../utils/types";
 import { getDataRatingAndDiscount } from "../../utils/helpers";
+import { categoryLinks } from "./categoryLinks";
 
 const Discount = () => {
   const data = useLoaderData() as Category[];
+
+  const { category } = useParams();
+  const linkArr = category ? categoryLinks[category] : [];
 
   const highDiscountProducts = getDataRatingAndDiscount(data, "discount");
 
@@ -14,31 +18,37 @@ const Discount = () => {
           With the biggest discount
         </h3>
         <div className="flex flex-wrap justify-center gap-4 lg:justify-start lg:gap-8 ">
-          {highDiscountProducts.map(
-            (product) =>
-              product && (
-                <Link
-                  key={product.id}
-                  to={product.id.toString()}
-                  className="group relative h-[350px] w-[250px] overflow-hidden rounded-xl bg-black-lighter shadow transition-transform focus:scale-105"
-                >
-                  <img
-                    alt={product.description}
-                    src={product.images[0]}
-                    height={300}
-                    className="h-[250px] w-full "
-                  />
-                  <p className=" p-6 text-center uppercase text-grey-lighter transition-colors group-hover:text-gold-dark ">
-                    {product.title}
-                  </p>
-                  <div className="absolute right-0 top-0 rounded-full bg-red-600/80 p-4">
-                    <p className="text-xl font-bold text-white">
-                      -{product.discountPercentage}%
+          {highDiscountProducts.map((product) => {
+            if (product) {
+              if (linkArr) {
+                const link = linkArr.find(
+                  (item) => item.fullName === product.category,
+                );
+                return (
+                  <Link
+                    key={product.id}
+                    to={`/${link?.link}/${product.id.toString()}`}
+                    className="group relative h-[350px] w-[250px] overflow-hidden rounded-xl bg-black-lighter shadow transition-transform focus:scale-105"
+                  >
+                    <img
+                      alt={product.description}
+                      src={product.images[0]}
+                      height={300}
+                      className="h-[250px] w-full "
+                    />
+                    <p className=" p-6 text-center uppercase text-grey-lighter transition-colors group-hover:text-gold-dark ">
+                      {product.title}
                     </p>
-                  </div>
-                </Link>
-              ),
-          )}
+                    <div className="absolute right-0 top-0 rounded-full bg-red-600/80 p-4">
+                      <p className="text-xl font-bold text-white">
+                        -{product.discountPercentage}%
+                      </p>
+                    </div>
+                  </Link>
+                );
+              }
+            }
+          })}
         </div>
       </section>
     );
