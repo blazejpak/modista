8f5b7f5f4b6872be02a8d0df7ef6c8f4a263bd72
blazejpak <blazej.pak@gmail.com>
2024-04-21 13:49:44 +0200

    Added SwiperJS

--- src/routes/ProductPage/SliderImage.tsx
+++ src/routes/ProductPage/SliderImage.tsx
@@ -18,30 +18,25 @@ type SliderImageProps = {
 
 const SliderImage = ({ images, alt, imageFunction }: SliderImageProps) => {
   return (
-    <div className=" flex ">
-      <Swiper
-        breakpoints={{
-          340: {
-            slidesPerView: 2,
-          },
-        }}
-        freeMode={true}
-        pagination={{ clickable: true }}
-        modules={[FreeMode, Pagination]}
-      >
-        {images &&
-          images.map((image, i) => (
-            <SwiperSlide key={i}>
-              <div
-                className={`mb-2 min-w-[70%] cursor-zoom-in  shadow-lg`}
-                onClick={() => imageFunction(true, i, "hidden")}
-              >
-                <img src={image} alt={alt} className="h-full object-cover" />
-              </div>
-            </SwiperSlide>
-          ))}
-      </Swiper>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      freeMode={false}
+      pagination={{ clickable: true }}
+      modules={[FreeMode, Pagination]}
+      className="max-w-[90vw]"
+    >
+      {images &&
+        images.map((image, i) => (
+          <SwiperSlide key={i}>
+            <div
+              className={`mb-10 flex min-w-[70%] cursor-zoom-in justify-center px-5`}
+              onClick={() => imageFunction(true, i, "hidden")}
+            >
+              <img src={image} alt={alt} className="h-full object-contain" />
+            </div>
+          </SwiperSlide>
+        ))}
+    </Swiper>
   );
 };
 
